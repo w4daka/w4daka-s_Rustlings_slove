1f9ab783981ca77fe63b66d845ff9a8960c1eed5
w4daka <ignorenegativeideology999@gmail.com>
2026-02-18 09:17:55 +0900

    move_semantics5.rs finished

--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -9,9 +9,11 @@ mod tests {
     #[test]
     fn move_semantics4() {
         let mut x = Vec::new();
-        let y = &mut x;
+        {
+            let y = &mut x;
+            y.push(42);
+        };
         let z = &mut x;
-        y.push(42);
         z.push(13);
         assert_eq!(x, [42, 13]);
     }
