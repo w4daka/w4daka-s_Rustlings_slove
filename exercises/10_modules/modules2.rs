4b573d9d012254a7ceaafe9bb2a5434b05134aac
w4daka <ignorenegativeideology999@gmail.com>
2026-02-21 13:35:24 +0900

    strings4.rs modules1.rs finished

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -3,8 +3,8 @@
 
 mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
+    use self::fruits::PEAR as ;
+    use self::veggies::CUCUMBER as ;
 
     mod fruits {
         pub const PEAR: &str = "Pear";
